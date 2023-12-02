342af7986c504fc92238767fb65a8c561cf329dc
Alessandro Carbone <alessandro.carbone@sncegroup.ch>
2023-12-02 16:27:45 +0100

    First part of day 1

---
+++ 2023/01/process.py
@@ -0,0 +1,27 @@
+import re
+
+def line_value(line):
+    line = line.replace("\n", "")
+    digits = re.sub("[^0-9]", "", line)
+    if len(digits) == 0:
+        return 0
+
+    if len(digits) == 1:
+        return int(digits[0] + digits[0])
+
+    return int(digits[0] + digits[-1])
+
+with open('input.txt') as f:
+    lines = f.readlines()
+    sum = 0
+
+    for line in lines:
+      sum += line_value(line)
+
+    print(sum)
+
+
+
+
+
+
